5485f85532325a0cc130f938388792de10b32149
Alessandro Zuppel <alessandrozuppel@libero.it>
2020-05-21 09:32:05 +0200

    .

--- Semafore.X/main.c
+++ Semafore.X/main.c
@@ -22,13 +22,13 @@
 #define CHS0 3
 #define ADFM 7
 
-char str[4]; //stringa di salvatagio per la conversione da int to string
+char str[4];                    //stringa di salvatagio per la conversione da int to string
 unsigned int count = 0;
-unsigned char update = 0; //variabile per aggiornare i valori sul men�
-char comando = 0;         //Prende il dato dalla seriale
+unsigned char update = 0;       //variabile per aggiornare i valori sul men�
+char comando = 0;               //Prende il dato dalla seriale
 unsigned char count_pwm = 0;
-char by1 = 0; //Primo byte ricevuto
-char by2 = 0; //Secondo byte ricevuto
+char by1 = 0;                   //Primo byte ricevuto
+char by2 = 0;                   //Secondo byte ricevuto
 unsigned char count_delay = 0;
 unsigned char Time_Red = 10;
 unsigned char Time_Yellow = 5;
@@ -66,8 +66,8 @@ void main(void)
 void init_ADC()
 {
     TRISA = 0xFF;   //imposto i pin come ingressi
-    ADCON0 = 0x00;  // setto ADCON0 00000000
-    ADCON1 = 0x80;  // SETTO ADCON1 (ADFM) a 1 --> risultato giustificato verso dx 10000000
+    ADCON0 = 0x00;  //setto ADCON0 00000000
+    ADCON1 = 0x80;  //SETTO ADCON1 (ADFM) a 1 --> risultato giustificato verso dx 10000000
     __delay_us(10); //delay condensatore 10us
 }
 
@@ -75,13 +75,12 @@ void init_ADC()
 int ADC_Read(char canale)
 {
     ADCON0 = (1 << ADON) | (canale << CHS0);
-    //ADCON0bits.ADON = 1;   //accendo il convertitore (ADCON0)
-    //ADCON0 |= canale << 3; //e setto il canale da convertire (ADCON0)
-    __delay_us(2); //attendo 1.6 uS
-    GO_nDONE = 1;  // avvio la conversione ADGO GO
-    while (GO_nDONE)
-        ;                          //attendo la fine della conversione
-    return ADRESL + (ADRESH << 8); // preparo il dato (valore = ADRESL + (ADREAH << 8)
+    //ADCON0bits.ADON = 1;               //accendo il convertitore (ADCON0)
+    //ADCON0 |= canale << 3;             //e setto il canale da convertire (ADCON0)
+    __delay_us(2);                       //attendo 1.6 uS
+    GO_nDONE = 1;                        // avvio la conversione ADGO GO
+    while (GO_nDONE);                    //attendo la fine della conversione
+    return ADRESL + (ADRESH << 8);       // preparo il dato (valore = ADRESL + (ADREAH << 8)
 }
 
 void intToString(int valore)
@@ -95,37 +94,36 @@ void intToString(int valore)
     str[3] = '\0';
 }
 
-double pow(double x, double n)
+double pow(double Base, double potenza)
 {
     int i;
     double number = 1;
-    for (i = 0; i < n; i++)
+    for (i = 0; i < potenza; i++)
     {
-        number *= x;
+        number *= Base;
     }
     return (number);
 }
 
 void UART_Init(int baudrate)
 {
-    TRISCbits.TRISC6 = 0; //TRISC= 0x80;   //10000000
+    TRISCbits.TRISC6 = 0;   //TRISC= 0x80;   //10000000
 
-    TXSTAbits.TXEN = 1; //TXSTA= 0x20;   //00100000
+    TXSTAbits.TXEN = 1;     //TXSTA= 0x20;   //00100000
 
-    RCSTAbits.SPEN = 1; //RCSTA= 0x90;   //10010000
-    RCSTAbits.CREN = 1; //RCSTA= 0x90;   //10010000
+    RCSTAbits.SPEN = 1;     //RCSTA= 0x90;   //10010000
+    RCSTAbits.CREN = 1;     //RCSTA= 0x90;   //10010000
 
     SPBRG = (_XTAL_FREQ / (long)(64UL * baudrate)) - 1;
-    INTCONbits.GIE = 1;  //abilito global interrupt
-    INTCONbits.PEIE = 1; //peripherial interrupt
-    PIE1bits.RCIE = 1;   //uart rx interrupt
+    INTCONbits.GIE = 1;     //abilito global interrupt
+    INTCONbits.PEIE = 1;    //peripherial interrupt
+    PIE1bits.RCIE = 1;      //uart rx interrupt
 }
 
 void UART_TxChar(char ch)
 {
-    while (!TXIF)
-        ;     //se TXIF ? a 0 la trasmissione ? ancora in corso
-    TXIF = 0; //lo resetto
+    while (!TXIF);      //se TXIF ? a 0 la trasmissione ? ancora in corso
+    TXIF = 0;           //lo resetto
     TXREG = ch;
 }
 
@@ -138,8 +136,7 @@ void UART_Write_Text(char *text)
 
 char UART_Read()
 {
-    while (!RCIF)
-        ;
+    while (!RCIF);
     RCIF = 0;
     return RCREG;
 }
@@ -169,9 +166,9 @@ void __interrupt() ISR()
     }
     if (TMR1IF == 1)
     {
-        //value = ~value; // complement the value for blinking the LEDs
-        TMR1H = 0x0B; // Load the time value(0xBDC) for 100ms delay
+        //value = ~value;   // complement the value for blinking the LEDs
+        TMR1H = 0x0B;       // Load the time value(0xBDC) for 100ms delay
         TMR1L = 0xDC;
-        TMR1IF = 0; // Clear timer interrupt flag
+        TMR1IF = 0;         // Clear timer interrupt flag
     }
 }
