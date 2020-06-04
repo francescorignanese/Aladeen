84c366755b620f553a355ea1bc6c7e732352043f
Simone <simone.ciancia@tecnicosuperiorekennedy.it>
2020-06-04 17:06:27 +0200

    aggiunta webapp

---
+++ WebApp/Semaforo/Semaforo/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BOP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return new RedirectResult("/Startseite");
+        }
+    }
+}
