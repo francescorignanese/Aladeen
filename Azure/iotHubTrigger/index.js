6eef7f03a8e44423bbbcba57a40ff7100622694c
Nicolò Garolla <nicologarolla@Nicolos-MacBook-Pro.local>
2020-07-11 21:24:42 +0200

    fix signalR connection

--- Azure/iotHubTrigger/index.js
+++ Azure/iotHubTrigger/index.js
@@ -2,7 +2,7 @@ module.exports = function (context, IoTHubMessages) {
     context.log(`JavaScript eventhub trigger function called for message array: ${IoTHubMessages[0]}`);
 
     context.bindings.signalRMessages = [{
-        "target": "message",
+        "target": "telemetry",
         "arguments": [ IoTHubMessages[0] ]
     }];
     
