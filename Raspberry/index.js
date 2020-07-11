2d0c01ef82ea04819405f231f86d3a93d6b9d7fc
Georgeta Pojoga <georgeta.pojoga@tecnicosuperiorekennedy.it>
2020-07-11 16:56:51 +0200

    fixed error on promises

--- Raspberry/index.js
+++ Raspberry/index.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
+var uuid = require('uuid');
 const SerialPort = require('serialport');
 const ByteLength = require('@serialport/parser-byte-length');
 const redis = require('redis');
 const port = new SerialPort('/COM7');
 const client = redis.createClient(6379, '192.168.0.99');
 const _ = require('lodash');
+
+//Connection to Redis
 client.on("error", (err) => {
 		console.log("error", err)
 });
@@ -16,9 +19,61 @@ client.on("ready", (err) => {
 	console.log("Redis ready to accept data!");
 });
 
-//..............JSON DATA................
+let corrupted = false;
+let date = (new Date(new Date().toString().split('GMT')[0]+' UTC').toISOString().split('.')[0]);
+let climate_received = false;
+let vehicle_received = false;
+let hasbeenSent = false;
+let trafficSent = false;
+let climateSent = false;
 
-//sensori atmosferici
+
+//-----------------------------------------------------------------------------------------------
+//RICHIESTA DATI TRAFFICO
+var sendDataRequestClimate = function() {
+	
+	let cmd_climate = [0x0A, 0x00, 0x00, 0x00, 0x00];
+	port.write(cmd_climate);
+	console.log('Sent value to Pic:', cmd_climate);
+	//hasbeenSent = true;
+	if(cmd_climate[0] === 10) {
+		climateSent = true;
+	}
+	cmd_climate = [0x00, 0x00, 0x00, 0x00, 0x00];
+	
+}
+
+var sendDataRequestTraffic = function() {
+	
+	let cmd_traffic = [0x08, 0x00, 0x00, 0x00, 0x00];
+	port.write(cmd_traffic);
+	console.log('Sent value to Pic:', cmd_traffic);
+	//hasbeenSent = true;
+	if (cmd_traffic[0] === 8) {
+		trafficSent = true;
+	}
+	cmd_traffic = [0x00, 0x00, 0x00, 0x00, 0x00];
+	
+}
+
+setInterval(() => {
+	sendDataRequestClimate();
+	if(climateSent) {
+		climateManagement();
+	}
+
+}, 50000);
+
+setInterval(() => {
+	sendDataRequestTraffic();
+	if(trafficSent) {
+		trafficManagement();
+	}
+}, 30000);
+
+//..............JSON OBJECTS................
+
+//Climate Sensors: temperature, humidity, pressure
 let json_climate = {
 	"description": "Data collection of the atmospheric sensors of the cross",
 	"sensor": "climate",
@@ -28,8 +83,7 @@ let json_climate = {
 	"data_climate": []
 };
 
-
-//JSON UNICO PER DATI TRAFFICO-SEMAFORI-STRADE
+//Traffic Sensors: for all roads, divided in 3 categories of vehicles: car, motorcycle, trucks
 let json_traffic = {
 	"description": "Data collection of the traffic from all the roads",
 	"sensor": "traffic",
@@ -39,70 +93,32 @@ let json_traffic = {
 	"data_carriers": []
 }
 
-
-let hasbeenSent = false;
-let trafficSent = false;
-let climateSent = false;
-let cmd;
-//-----------------------------------------------------------------------------------------------
-//RICHIESTA DATI TRAFFICO
-var sendToPic = async function() {
- 
-	if (!hasbeenSent) {
-		cmd = [0x08, 0x00, 0x00, 0x00, 0x00];
-		port.write(cmd);
-		console.log('Sent value to Pic:', cmd);
-		hasbeenSent = true;
-		if(cmd[0] === 10) {
-			climateSent = true;
-		} else if (cmd[0] === 8) {
-			trafficSent = true;
-		}
-	}
-	return hasbeenSent;
-}
-
-
-if(!hasbeenSent) {
-	sendToPic();
-	console.log('message written');
-}
-
-//RICEVO I DATI E LI PACCHETTIZZO
-var byteReceive = new Promise(function(resolve, reject) { 
-	let arrayBinary = [];
-	const parser = port.pipe(new ByteLength({length: 5}));
-	parser.on('data', (data) => { 
-		let msgSize = data.length;
-		for (let i = 0; i < msgSize; i++) {
-			let valore = parseInt(data[i], 10).toString(2);
-			//aggiungiamo gli 0 omessi perchè non significanti
-			let binary = valore.padStart(8, '0');
-			arrayBinary.push(binary);
-			//console.log(arrayBinary);
-		}
-
-		let packets = _.chunk(arrayBinary, 5)
-		//console.log(packets);
-		//console.log('----------------------');
-
-		if(climateSent && packets.length > 2) {
-			resolve(packets);
-		} else if (trafficSent && packets.length > 11) {
-			resolve(packets);
-		}
-		
-	});
-});
-
-let corrupted = false;
-let date = (new Date(new Date().toString().split('GMT')[0]+' UTC').toISOString().split('.')[0]);
-let climate_received = false;
-let vehicle_received = false;
-
-
 function climateManagement() {
-    
+
+	var byteReceive = new Promise(function(resolve, reject) { 
+		let arrayBinary = [];
+		const parser = port.pipe(new ByteLength({length: 5}));
+		parser.on('data', (data) => { 
+			let msgSize = data.length;
+			for (let i = 0; i < msgSize; i++) {
+				let valore = parseInt(data[i], 10).toString(2);
+				//aggiungiamo gli 0 omessi perchè non significanti
+				let binary = valore.padStart(8, '0');
+				arrayBinary.push(binary);
+				//console.log(arrayBinary);
+			}
+	
+			let packets = _.chunk(arrayBinary, 5)
+			//console.log(packets);
+			//console.log('----------------------');
+	
+			if(climateSent && packets.length > 2) {
+				resolve(packets);
+			}
+			
+		});
+	});
+
     byteReceive.then(function(returnValue) {
         returnValue.forEach(pack => {
 			console.log('pack', pack);
@@ -123,7 +139,6 @@ function climateManagement() {
 				"type": "",  //temp, humidity, pressure
 				"value": 0
 			}
-
 			if (byte1_sa === '0' && !corrupted) { //se è un sensore
 		
 				//SE NEL BYTE 1 ARRIVANO ID RIFERITI A SENSORI ATMOSFERICI
@@ -150,7 +165,6 @@ function climateManagement() {
 						break;
 					}	
 				}
-				
 			}
 			pushClimateOnRedis(json_weather);
 		});
@@ -159,8 +173,6 @@ function climateManagement() {
         console.log(err);
     });
 }
-
-
 
 function pushClimateOnRedis(json_weather) {
 	let json_string;
@@ -173,10 +185,33 @@ function pushClimateOnRedis(json_weather) {
 		});
 		fs.writeFileSync("climate.json", json_string);
 	}
-	
 }
 
 function trafficManagement() {
+	var byteReceive = new Promise(function(resolve, reject) { 
+		let arrayBinary = [];
+		const parser = port.pipe(new ByteLength({length: 5}));
+		parser.on('data', (data) => { 
+			let msgSize = data.length;
+			for (let i = 0; i < msgSize; i++) {
+				let valore = parseInt(data[i], 10).toString(2);
+				//aggiungiamo gli 0 omessi perchè non significanti
+				let binary = valore.padStart(8, '0');
+				arrayBinary.push(binary);
+				//console.log(arrayBinary);
+			}
+	
+			let packets = _.chunk(arrayBinary, 5)
+			//console.log(packets);
+			//console.log('----------------------');
+	
+			if (trafficSent && packets.length > 11) {
+				resolve(packets);
+			} else {
+				console.log('ERROR: Invalid format packets received from PIC');
+			}
+		});
+	});
 	byteReceive.then(function(returnValue) {
 		let byte2_full = false;
 		let byte2_type;
@@ -203,7 +238,7 @@ function trafficManagement() {
 			let value = clean_value4.concat(clean_value3);
 			decimal = parseInt(value, 2); //parso il binario in decimale
 			console.log(decimal);
-
+			
 			let json_carrier = {
 				"id_road": 0, //1, 2, 3, 4 
 				"id_semaphores": 0, //coppia 0 o 1
@@ -214,65 +249,56 @@ function trafficManagement() {
 				
 				switch (byte1_id) {
 					case '0001': {
-						
 						console.log('Semaforo 1');
 						json_carrier.id_road = 1;
 						json_carrier.id_semaphores = 1;
-						
-						
 						break;
 					}
 					case '0010': {
-						
 							console.log('Semaforo 2');
 							json_carrier.id_road = 2;
 							json_carrier.id_semaphores = 0;
-							
-						
 						break;
 					}
 					case '0011': {
-						
 							console.log('Semaforo 3');
 							json_carrier.id_road = 3;
 							json_carrier.id_semaphores = 1;
-							
-						
 						break;
 					}
 					case '0100': {
-						
 							console.log('Semaforo 4');
 							json_carrier.id_road = 4;
 							json_carrier.id_semaphores = 0;
-							isFirstTime = false;
-						
 						break;
 					}
 				}
-				
-				
 			}
-				let prova = vehicleDistribution(byte2_full, byte2_type, decimal);
-				let isPresent = false;
-				json_traffic.data_carriers.forEach(carrier => {
-					if(carrier.id_road === json_carrier.id_road) {
-						isPresent = true;
-						carrier.data_vehicles.push(prova);
-					}
-				});
-
-				if(!isPresent) {
-					json_carrier.data_vehicles.push(prova);
-					json_traffic.data_carriers.push(json_carrier);
-				}
-				
-				json_string = JSON.stringify(json_traffic);
-				console.log(json_traffic);
-				fs.writeFileSync("traffic.json", json_string);
+			let vehicle = vehicleDistribution(byte2_full, byte2_type, decimal);
+			pushTrafficOnRedis(vehicle, json_carrier);
 		});	
 			
 	});
+}
+
+function pushTrafficOnRedis(vehicle, json_carrier) {
+	
+	let isPresent = false;
+	json_traffic.data_carriers.forEach(carrier => {
+		if(carrier.id_road === json_carrier.id_road) {
+			isPresent = true;
+			carrier.data_vehicles.push(vehicle);
+		}
+	});
+
+	if(!isPresent) {
+		json_carrier.data_vehicles.push(vehicle);
+		json_traffic.data_carriers.push(json_carrier);
+	}
+	
+	json_string = JSON.stringify(json_traffic);
+	console.log(json_traffic);
+	fs.writeFileSync("traffic.json", json_string);
 }
 
 
@@ -308,12 +334,4 @@ function vehicleDistribution(byte2_full, byte2_type, decimal) {
 		}
 		return json_vehicle;
 	}
-}
-
-if(climateSent) {
-	climateManagement();
-}
-
-if(trafficSent) {
-	trafficManagement();
 }
